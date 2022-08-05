2d5b43d95256bb674849b827ca0f864c472b4eda
augusto <augustoclimak3@gmail.com>
2022-08-04 21:16:49 -0300

    desafio dio

---
+++ modulo II/4-Introdução ao javascript/variaveis com javascript/index.js
@@ -0,0 +1,11 @@
+var firstName = 'joão';
+var lastNmae = 'souza';
+
+if(firstName === 'joão'){
+    var firstName = 'pedro';
+    let lastNmae = ' Silva';
+
+    console.log(firstName, lastNmae);
+ }
+
+ console.log(firstName, lastNmae);
