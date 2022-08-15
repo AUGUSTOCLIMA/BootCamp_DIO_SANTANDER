3939d5f6d96f16ab5404a243105e8b242c87b70b
augusto <augustoclimak3@gmail.com>
2022-08-15 17:11:54 -0300

    modulos javascript

---
+++ modulo II/9-modulos/main.js
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.js";
+
+console.log(mostraCidade("augusto", "taruma"));
+console.log(mostraIdade("augusto", 30));
+console.log(mostraHobby("ana", "game"));
